4932e3132e3d603d620b025342f641ce502470a2
Erik Hulinian <gulinianerik2@gmail.com>
2023-11-22 17:59:01 +0200

    Ваш описовий коміт-повідомлення

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const x1 = 42;
+
+const x2 = 0b101010;
+
+const x3 = 0o52;
+
+const x4 = 0x2A;
+
+const x5 = 0x2a;
+
+console.log(x1, x2, x3, x3, x4, x5)
+
+let y1 = 42.000;
+
+let y2 = 42.;
+
+let y3 = 0.42e2;
+
+let y4 = .42e2;
+
+let y5 = 42000e-3
+
+console.log(y1, y2, y3, y4, y5);
